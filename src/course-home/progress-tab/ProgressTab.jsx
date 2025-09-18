29d2884698591a8f1dda69a10a20c0719baccdf2
LuisAbonce <ltorresa1701@alumno.ipn.mx>
2025-09-18 11:41:26 -0600

    feat: remove side panel for improved layout and focus on main content

--- src/course-home/progress-tab/ProgressTab.jsx
+++ src/course-home/progress-tab/ProgressTab.jsx
@@ -37,10 +37,6 @@ const ProgressTab = () => {
         </div>
 
         {/* Side panel */}
-        <div className="col-12 col-md-4 p-0 px-md-4">
-          {wideScreen && <CertificateStatus />}
-          {/* <RelatedLinks /> - Comentado para ocultar Related Links */}
-        </div>
       </div>
     </>
   );
